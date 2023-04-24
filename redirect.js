b6459bb47c86601cd1907d1f4fff096ce678e86c
William Harrison <william@williamdavidharrison.com.au>
2023-04-24 16:52:51 +0800

    Create redirect.js

---
+++ redirect.js
@@ -0,0 +1,41 @@
+const urls = [
+    "https://bots.wdh.gg",
+    "https://delete-webhooks.wdh.gg",
+    "https://easyscript.dev",
+    "https://forkedgames.github.io",
+    "https://free-for.life",
+    "https://freesubdomains.org",
+    "https://github.com/WilliamDavidHarrison/coming-soon",
+    "https://github.com/WilliamDavidHarrison/contact-form",
+    "https://github.com/WilliamDavidHarrison/endless-explorer",
+    "https://github.com/WilliamDavidHarrison/hello-world-extension",
+    "https://github.com/WilliamDavidHarrison/html-404-page",
+    "https://github.com/WilliamDavidHarrison/http-forwarder",
+    "https://github.com/WilliamDavidHarrison/infinite-users",
+    "https://github.com/WilliamDavidHarrison/restart.exe",
+    "https://github.com/WilliamDavidHarrison/text-card",
+    "https://github.com/WilliamDavidHarrison/url-shortener",
+    "https://github.com/cmd-protocol/cmd-protocol",
+    "https://github.com/wdhdev/blooket-hacks",
+    "https://github.com/wdhdev/file-sorter",
+    "https://github.com/wdhdev/online-terminal",
+    "https://github.com/wdhdev/pi-calculator",
+    "https://github.com/wdhdev/pinger",
+    "https://github.com/wdhdev/technobot",
+    "https://github.com/wdhdev/validate-email",
+    "https://github.com/wdhdev/validate-url",
+    "https://github.com/wdhdev/workflows",
+    "https://github.com/wdhdev/ytdl-script",
+    "https://gravatar.wdh.gg",
+    "https://guthib.wdh.gg",
+    "https://pi.wdh.gg",
+    "https://qrcode.wdh.gg",
+    "https://wdh.gg",
+    "https://wharchive.org",
+    "https://william.net.au",
+    "https://williamharrison.dev"
+]
+
+const number = Math.round(Math.random() * urls.length);
+
+window.location.replace(urls[number]);
